dbbe038d6202b083e89964a34e148a2c6a41e8e8
nuhptr <nugrahaadi733@gmail.com>
2023-12-24 09:39:32 +0700

    twitter/apply-event

--- Twitter.sol
+++ Twitter.sol
@@ -2,17 +2,54 @@
 pragma solidity ^0.8.0;
 
 contract Twitter {
+    //* add event logging
+    event TweetCreated(
+        uint256 id,
+        address author,
+        string content,
+        uint256 timestamp
+    );
+    event TweetLiked(
+        address liker,
+        address tweetAuthor,
+        uint256 tweetId,
+        uint256 newLike
+    );
+    event TweetUnliked(
+        address unliker,
+        address tweetAuthor,
+        uint256 tweetId,
+        uint256 newLike
+    );
+
     //* adding structs
     struct Tweet {
+        //* add id
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
         uint256 likes;
     }
 
-    uint16 constant MAX_TWEETS_LENGTH = 140;
+    uint16 MAX_TWEETS_LENGTH = 140;
+
     //* adding mapping
     mapping(address => Tweet[]) public tweets;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function changeTweetLength(uint16 newTweetLength) public {
+        MAX_TWEETS_LENGTH = newTweetLength;
+    }
 
     //* adding function to create tweets
     function createTweets(string memory _tweet) public {
@@ -23,6 +60,7 @@ contract Twitter {
         );
 
         Tweet memory newTweet = Tweet({
+            id: tweets[msg.sender].length,
             author: msg.sender,
             content: _tweet,
             timestamp: block.timestamp,
@@ -30,6 +68,32 @@ contract Twitter {
         });
 
         tweets[msg.sender].push(newTweet);
+
+        emit TweetCreated(
+            newTweet.id,
+            newTweet.author,
+            newTweet.content,
+            newTweet.timestamp
+        );
+    }
+
+    //* adding function to like tweets for universal user
+    function likeTweet(address author, uint256 _id) external {
+        require(tweets[author][_id].id == _id, "Tweet does not exist");
+
+        tweets[author][_id].likes++;
+
+        emit TweetLiked(msg.sender, author, _id, tweets[author][_id].likes);
+    }
+
+    //* adding function to unlike tweets for universal user
+    function unlikeTweet(address author, uint256 _id) external {
+        require(tweets[author][_id].id == _id, "Tweet does not exist");
+        require(tweets[author][_id].likes > 0, "Tweet has no likes");
+
+        tweets[author][_id].likes--;
+
+        emit TweetUnliked(msg.sender, author, _id, tweets[author][_id].likes);
     }
 
     //* adding function to get tweet
